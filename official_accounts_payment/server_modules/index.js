2c2864b7eacfeb748d379bfb26ae8c160a7f5727
CRONWMMM <kimmmiy@sina.cn>
2018-01-12 00:21:35 +0800

    完善微信公众号支付后台

--- official_accounts_payment/server_modules/index.js
+++ official_accounts_payment/server_modules/index.js
@@ -1,2 +1,18 @@
 
+const CONFIG = require('../common/config').CONFIG
+const api = CONFIG.api
 
+let demo = `<xml>
+               <appid>${CONFIG.appid}</appid>
+               <mch_id>${CONFIG.mch_id}</mch_id>
+               <body>JSAPI支付测试</body>
+               <sign>0CB01533B8C1EF103065174F50BCA001</sign>
+               <sign_type>MD5</sign_type>
+               <nonce_str>1add1a30ac87aa2db72f57a2375d8fec</nonce_str>
+               <notify_url>http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php</notify_url>
+               <openid>oUpF8uMuAJO_M2pxb1Q9zNjWeS6o</openid>
+               <out_trade_no>1415659990</out_trade_no>
+               <spbill_create_ip>14.23.150.211</spbill_create_ip>
+               <total_fee>1</total_fee>
+               <trade_type>JSAPI</trade_type>
+            </xml>`
