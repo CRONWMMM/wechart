32730d427e41812f868ff44d28165ba1ad754f22
CRONWMMM <kimmmiy@sina.cn>
2018-01-11 23:55:15 +0800

    完善微信公众号支付后台

--- official_accounts_payment/common/config.js
+++ official_accounts_payment/common/config.js
@@ -2,14 +2,13 @@
  * config
  * @type {{common: {baseUrl: string, appid: string, secret: string}, client: {}, server: {}}}
  */
+
 const CONFIG = {
-	common: {
-		baseUrl: '',
-		appid: '',
-		secret: ''
-	},
-	client: {},
-	server: {}
+    api: 'https://api.mch.weixin.qq.com/pay/unifiedorder',		// api
+    appid: '',													// 微信开放平台审核通过的应用APPID
+    mch_id: '',													// 微信支付分配的商户号
+    notify_url: '',												// 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
+    trade_type: 'JSAPI'											// 交易类型 JSAPI--公众号支付
 }
 
 
